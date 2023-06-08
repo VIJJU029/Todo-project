07fedbec44c1f077ade6b71477da04ad15bd41b9
Praveena Roja <vijayvasudev029@gmail.com>
2023-06-08 08:00:04 +0530

    this is my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,117 @@
-import logo from './logo.svg';
+
+//Use state hooks : it is used  to manage your state{data} in functional component
+// declare hooks at the top level
+//import usestate from 'react'
+// const count = useState(intial value)
+// usestate = variable, udating function
+
+
+
+
 import './App.css';
 
+import { useState,useRef } from 'react';
+
+// state : Data
+// uncontrolled {useref} vs controlled{use state}
+
+
 function App() {
+  const [list,setList]= useState([]);
+  const [newtask,setNewTask] = useState('');
+  const [search,setSearch] = useState('');
+  //  console.log("re-render")
+
+  // const newinput =(e)=>{
+
+  //   setNewTask(e.target.value)
+
+  // }
+
+  const addtask =(e)=>{
+
+    localStorage.setItem('lists',JSON.stringify([...list, newtask]))
+     setList([...list, newtask])
+     setNewTask(
+      ''
+     )
+
+    // setNewTask("")
+  }
+
+  const deleteTask =(i)=>{
+    const dellist =[...list];
+    dellist.splice(i,1)
+    setList(dellist)
+    localStorage.setItem('lists',dellist)
+  }
+  const updateTask =(e,i)=>{
+    const uptask =[...list];
+    uptask.splice(i,1,e.target.value) 
+    setList(uptask)
+    localStorage.setItem('lists',uptask)
+
+  }
+  
+
+  const searchfun =(val)=>{
+    setSearch(val)
+
+    
+  }
+  const keyEnter =(e)=>{
+    if(e.key ==="Enter"){
+      addtask()
+
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
+    <div className ="search">
+    <input type='text' placeholder='search Task🔍' onChange={(e)=>{searchfun(e.target.value)}}></input>
     </div>
+    
+
+
+
+      <h1 className = "heading">To do App ⚡</h1>
+   <div className ="inputs" >
+
+  <input type = "text"
+   onChange={(e)=>{setNewTask(e.target.value)}} 
+   onKeyDown={keyEnter} value ={newtask} />
+  <button className = "btn" onClick={addtask}>Add task👍</button>
+
+ </div>
+
+    <div className = "container">
+
+      {
+        list.map((val,i)=>{
+        if(val.toLowerCase().includes(search.toLowerCase())){
+          return(
+            <div className ="list" key={i} >
+        <input type ="text" value ={val}></input>
+        <span className ="icon" onClick={()=>{deleteTask()}}
+        >❌</span>
+      </div>
+
+          )
+          
+        }
+
+          
+        })
+
+      }
+      
+
+    </div>
+      
+      </div>
+    
   );
 }
 
